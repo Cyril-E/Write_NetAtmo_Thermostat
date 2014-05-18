14dadfff50ef2b99a5fcc6f4c382f0dee6dd1830
Cyril <github@ituilerie.com>
2014-05-18 08:55:03 +0200

    6st Commit
    écriture de la consigne de température et de la durée de celle-ci

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ http://xxxxxxxxx/thermostat_write.php?mode=off                     pour l'arret
 http://xxxxxxxxx/thermostat_write.php?mode=program           pour passer en mode programme
 http://xxxxxxxxx/thermostat_write.php?mode=away                pour passer en mode absent
 http://xxxxxxxxx/thermostat_write.php?mode=hg                    pour passer en mode hors gel
+http://xxxxxxxxx/thermostat_write.php?mode=max&length=120           pour passer en mode max pendant un certain temps (en minutes)  ici 120 minutes
+http://xxxxxxxxx/thermostat_write.php?mode=manual&length=120&consigne=24           pour passer en mode manuel pendant un certain temps (en minutes)  ici 120 minutes a 24°c
--- thermostat_write.php
+++ thermostat_write.php
@@ -8,11 +8,13 @@
 // http://xxxxxxxxx/thermostat_write.php?mode=program           pour passer en mode programme
 // http://xxxxxxxxx/thermostat_write.php?mode=away           pour passer en mode absent
 // http://xxxxxxxxx/thermostat_write.php?mode=hg           pour passer en mode hors gel
+// http://xxxxxxxxx/thermostat_write.php?mode=max&length=120           pour passer en mode max pendant un certain temps (en minutes)  ici 120 minutes
+// http://xxxxxxxxx/thermostat_write.php?mode=manual&length=120&consigne=24           pour passer en mode manuel pendant un certain temps (en minutes)  ici 120 minutes a 24°c
 
 
 $mode=$_GET['mode'];
-//$consigne=$_GET['consigne'];
-//$endtime=$_GET['fin'];
+$length=$_GET['length'];
+$consigne=$_GET['consigne'];
 
 $password='xxxx';
 $username='xxxx';
@@ -60,7 +62,32 @@ $module2 = $json_devices["body"]["modules"][1]["_id"];
 $device3 = $json_devices["body"]["devices"][2]["_id"];
 $module3 = $json_devices["body"]["modules"][2]["_id"];
 
-$url="/api/setthermpoint?access_token=" . $params['access_token']."&device_id=".$device1."&module_id=".$module1."&setpoint_mode=".$mode;
+    
+if ($mode=="off") {
+	
+    $url="/api/setthermpoint?access_token=" . $params['access_token']."&device_id=".$device1."&module_id=".$module1."&setpoint_mode=".$mode;
+    
+} elseif ($mode=="hg") {
+	
+		$url="/api/setthermpoint?access_token=" . $params['access_token']."&device_id=".$device1."&module_id=".$module1."&setpoint_mode=".$mode;
+	
+} elseif ($mode=="away") {    
+ 
+    $url="/api/setthermpoint?access_token=" . $params['access_token']."&device_id=".$device1."&module_id=".$module1."&setpoint_mode=".$mode;
+    
+} elseif ($mode=="max") {
+	
+			$endtime = time() + ($length * 60);
+      $url="/api/setthermpoint?access_token=" . $params['access_token']."&device_id=".$device1."&module_id=".$module1."&setpoint_mode=".$mode."&setpoint_endtime=".$endtime;      
+    
+} else {
+	
+			$endtime = time() + ($length * 60);
+    	$url="/api/setthermpoint?access_token=" . $params['access_token']."&device_id=".$device1."&module_id=".$module1."&setpoint_mode=".$mode."&setpoint_endtime=".$endtime."&setpoint_temp=".$consigne;      
+    
+}
+
+
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, "http://api.netatmo.net".$url);
